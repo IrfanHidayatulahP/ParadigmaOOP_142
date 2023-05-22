b44f9781ed42a79cca000e9faeef699d6fe2cb3f
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-22 11:41:11 +0700

    membuat inheritance pada objek pelajar dengan orang

--- Pewarisan/Pewarisan.cpp
+++ Pewarisan/Pewarisan.cpp
@@ -19,6 +19,26 @@ public:
     }
 };
 
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) :
+        orang(pNama),
+        sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string Perkenalan() {
+        return "Hallo, nama saya" + nama + "Dari Sekolah" + sekolah + "\n\n";
+    }
+};
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
