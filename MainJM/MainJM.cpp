5718b6861e441436f0b76269ec99a680c6b21510
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-22 17:36:35 +0700

    membuat main program pada objek Manusia

--- MainJM/MainJM.cpp
+++ MainJM/MainJM.cpp
@@ -2,10 +2,11 @@
 #include <string>
 using namespace std;
 
-
-
-
+#include "Jantung.h"
+#include "Manusia.h"
 
 int main() {
-
+	manusia* varManusia = new manusia("Jono");
+	delete varManusia;
+	return 0;
 }
