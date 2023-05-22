3612334083b244293b8bcefac8919262133c725b
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-22 15:47:13 +0700

    membuat main program dokter dan pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -59,9 +59,26 @@ void dokter::cetakPasien() {
 }
 
 
+int main() {
+    dokter* varDokter1 = new dokter("dr. Budi");
+    dokter* varDokter2 = new dokter("dr. Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    varDokter1->tambahPasien(varPasien1);
+    varDokter2->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
 }
 
